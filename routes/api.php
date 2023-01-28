18efc8a2750fed3aa76a53ceeeec4768bc4e0846
Doniyor <doniyor199696@gmail.com>
2023-01-28 13:09:45 +0600

    move api categoryController

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,6 @@
 <?php
 
-use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\Api\CategoryController;
 use App\Http\Controllers\TestController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
