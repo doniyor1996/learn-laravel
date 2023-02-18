c2dfb25d93e70c8576bcc799d62268f13382f392
Doniyor <doniyor199696@gmail.com>
2023-02-18 10:33:50 +0600

    View stylization

--- resources/views/categories/index.blade.php
+++ resources/views/categories/index.blade.php
@@ -1,12 +1,20 @@
-@extends('layouts.app')
-
-@section('content')
-    <div class="container mx-auto">
-        <h3>Categories</h3>
-        <ul class="list-disc">
-            @foreach ($categories as $c)
-                <li><a href="/categories/{{$c['id']}}/products">{{ $c['name'] }}</a></li>
-            @endforeach
-        </ul>
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            Categories
+        </h2>
+    </x-slot>
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 text-gray-900">
+                    <ul class="list-disc">
+                        @foreach ($categories as $c)
+                            <li><a href="/categories/{{$c['id']}}/products">{{ $c['name'] }}</a></li>
+                        @endforeach
+                    </ul>
+                </div>
+            </div>
+        </div>
     </div>
-@endsection
+</x-app-layout>
--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -1,12 +1,20 @@
-@extends('layouts.app')
-
-@section('content')
-    <div class="container mx-auto">
-        <h1>Products</h1>
-        <ul class="list-disc">
-            @foreach ($list as $product)
-                <li><a href="/products/{{$product['id']}}">{{ $product['name'] }}</a></li>
-            @endforeach
-        </ul>
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            Products
+        </h2>
+    </x-slot>
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 text-gray-900">
+                    <ul class="list-disc">
+                        @foreach ($list as $product)
+                            <li><a href="/products/{{$product['id']}}">{{ $product['name'] }}</a></li>
+                        @endforeach
+                    </ul>
+                </div>
+            </div>
+        </div>
     </div>
-@endsection
+</x-app-layout>
